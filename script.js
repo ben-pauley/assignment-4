052c0044841e7a781d64e47a2fde3f38bae344d0
Ben <pauleybj@gmail.com>
2021-01-08 14:16:47 +0000

    button successfully responds to event listener to change html content

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("startQuizBtn").addEventListener("click", question1);
+
+function question1(){
+    document.getElementById("questionHead").innerHTML = "This is the first question.";
+}
