5cb199d8a1c4846d1e31bf9daac4892f1f0df9a2
Ben <pauleybj@gmail.com>
2021-01-08 23:21:01 +0000

    program can now cycle through five question template pages and finish at game over page

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-document.getElementById("startQuizBtn").addEventListener("click", question1);
+document.getElementById("startQuizBtn").addEventListener("click", beginQuiz);
 
 var a = document.getElementById("highscoresLink");
 a.addEventListener("click", highscores);
+
+function beginQuiz(){
+    startTimer();
+    generateQuestion(1);
+}
 
 function startTimer(){
     var seconds = 60;
@@ -9,18 +14,18 @@ function startTimer(){
     var timer = setInterval(function(){
         seconds--;
         document.getElementById("timer").textContent = "Time: " + seconds;
-        if (seconds <=0) clearInterval(timer);
+        if ((seconds <= 0) || (finalQuestionAnswered() === true)){
+            clearInterval(timer);
+            gameOver();
+        }
     }, 1000);
 }
 
-function question1(){
-    startTimer();
+function generateQuestion(questionNumber){
     //the main div to hold everything question related
     var mainDiv = document.getElementById("mainDiv");
     //hide previous body content
-    document.getElementById("title").hidden = true;
-    document.getElementById("instructions").hidden = true;
-    document.getElementById("startQuizBtnDiv").hidden = true;
+    mainDiv.textContent = "";
     //create each of the elements
     var questionHead = document.createElement("h3");
     var option1Div = document.createElement("div");
@@ -31,12 +36,57 @@ function question1(){
     var option2 = document.createElement("button");
     var option3 = document.createElement("button");
     var option4 = document.createElement("button");
+
+    var questions = ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5"];
+    var q1Answers = ["Wrong", "Correct", "Wrong", "Wrong"]
+    var q2Answers = ["Wrong", "Wrong", "Wrong", "Correct"]
+    var q3Answers = ["Correct", "Wrong", "Wrong", "Wrong"]
+    var q4Answers = ["Wrong", "Correct", "Wrong", "Wrong"]
+    var q5Answers = ["Wrong", "Wrong", "Correct", "Wrong"]
+
     //add content to elements
-    questionHead.textContent = "This is the first question";
-    option1.textContent = "Option 1";
-    option2.textContent = "Option 2";
-    option3.textContent = "Option 3";
-    option4.textContent = "Option 4";
+    questionHead.textContent = questions[questionNumber - 1];
+
+    function finalQuestionAnswered(){
+        var finalQuestion = false;
+        if (questions[questionNumber] === questions[4]){
+            finalQuestion = true;
+        }
+        return finalQuestion;
+    }
+    
+    switch (questions[questionNumber - 1]) {
+        case questions[0]:
+            option1.textContent = q1Answers[0];
+            option2.textContent = q1Answers[1];
+            option3.textContent = q1Answers[2];
+            option4.textContent = q1Answers[3];
+            break;
+        case questions[1]:
+            option1.textContent = q2Answers[0];
+            option2.textContent = q2Answers[1];
+            option3.textContent = q2Answers[2];
+            option4.textContent = q2Answers[3];
+            break;
+        case questions[2]:
+            option1.textContent = q3Answers[0];
+            option2.textContent = q3Answers[1];
+            option3.textContent = q3Answers[2];
+            option4.textContent = q3Answers[3];
+            break;
+        case questions[3]:
+            option1.textContent = q4Answers[0];
+            option2.textContent = q4Answers[1];
+            option3.textContent = q4Answers[2];
+            option4.textContent = q4Answers[3];
+            break;
+        case questions[4]:
+            option1.textContent = q5Answers[0];
+            option2.textContent = q5Answers[1];
+            option3.textContent = q5Answers[2];
+            option4.textContent = q5Answers[3];
+            break;
+    }
     //add ids to elements
     questionHead.setAttribute("id", "questionHead");
     option1.setAttribute("id", "option1");
@@ -68,37 +118,37 @@ function question1(){
     mainDiv.appendChild(option2Div);
     mainDiv.appendChild(option3Div);
     mainDiv.appendChild(option4Div);
-    //if a button is clicked
-    option1.addEventListener("click", gameOver);
-    option2.addEventListener("click", gameOver);
-    option3.addEventListener("click", gameOver);
-    option4.addEventListener("click", gameOver);
-}
-
-function question2(){
     
-}
-
-function question3(){
+    var newQuestionNumber = questionNumber + 1;
     
-}
-
-function question4(){
-    
-}
-
-function question5(){
-    
+    if (newQuestionNumber !== 6)
+    {
+        option1.addEventListener("click", function(){
+            generateQuestion(newQuestionNumber);
+        });
+        option2.addEventListener("click", function(){
+            generateQuestion(newQuestionNumber);
+        });
+        option3.addEventListener("click", function(){
+            generateQuestion(newQuestionNumber);
+        });
+        option4.addEventListener("click", function(){
+            generateQuestion(newQuestionNumber);
+        });
+    }
+    else
+    {
+        option1.addEventListener("click", gameOver);
+        option2.addEventListener("click", gameOver);
+        option3.addEventListener("click", gameOver);
+        option4.addEventListener("click", gameOver);
+    }
 }
 
 function gameOver(){
     var mainDiv = document.getElementById("mainDiv");
 
-    document.getElementById("questionHead").hidden = true;
-    document.getElementById("option1").hidden = true;
-    document.getElementById("option2").hidden = true;
-    document.getElementById("option3").hidden = true;
-    document.getElementById("option4").hidden = true;
+    mainDiv.textContent = "";
 
     var gameOverTitle = document.createElement("h3");
     var finalScore = document.createElement("p");
@@ -115,6 +165,8 @@ function gameOver(){
     mainDiv.appendChild(gameOverTitle);
     mainDiv.appendChild(finalScore);
     mainDiv.appendChild(submitScore);
+
+    var score = document.getElementById(timer).textContent;
 }
 
 function highscores(){
